c81c51fc6908a99f442fd32785bfc6f43f2d1325
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2024-01-18 00:52:41 +0700

    update

--- android/app/src/main/kotlin/com/example/standzclient/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.standzclient
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/service/dio.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-
-Dio dio() {
-  Dio dio = new Dio();
-  dio.options.baseUrl = 'http://127.0.0.1:8000/api/v1';
-  dio.options.headers['accept'] = 'Application/json';
-  return dio;
-}
